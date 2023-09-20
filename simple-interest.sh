4034cd29c4e7d1bb0b44bdace6c1321603ddf04a
stefanodipierro <51507204+stefanodipierro@users.noreply.github.com>
2023-09-20 15:17:11 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # stefanodipierro
    # Input:
    # p, principal amount
    # t, time period in years
